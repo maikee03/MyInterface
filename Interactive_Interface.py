82032bbdbeaf1d828764984cfe29f2f1880be420
maikee03 <maike.hermle@web.de>
2025-11-28 12:34:14 +0100

    V06

--- Interactive_Interface.py
+++ Interactive_Interface.py
@@ -5,39 +5,33 @@ import plotly.express as px
 st.set_page_config(layout="wide")
 st.title("Interaktive Finanzübersicht")
 
-################ SLIDER INPUT ################
 
-#Einkommen
-
-
-#Ausgaben
-
-
-################# Calculations #################
-
-#Bilanz
-
-
-################# Output #################
-st.header("Jahres Bilanz")
 col1, col2 = st.columns(2)
+############ Einnahmen ############
 with col1:
-    Gesamtes_Einkommen = st.slider("Gesamtes Einkommen", 0, 7000, 2000)
-    
+    st.header("Einnahmen")
+    ## Interaktive Slider für Einnahmen
+    Gesamtes_Einkommen = st.slider("Gesamte Einnahmen", 0, 7000, 2000)
+
     st.metric("Gesamtes Einkommen", Gesamtes_Einkommen)
 
+############ Ausgaben ############
 with col2:
+    st.header("Ausgaben")
+    ## Interaktive Slider für Ausgaben
     Projekte = st.slider("Projektausgaben", 0, 2000, 800)
     Verwaltung = st.slider("Verwaltungsausgaben", 0, 1000, 350)
     Werbung = st.slider("Werbungsausgaben", 0, 500, 60)
     
     Gesamte_Ausgaben = Projekte + Verwaltung + Werbung
     st.metric("Gesamte Ausgaben", Gesamte_Ausgaben)
+    Ersparnisse = Gesamtes_Einkommen - Gesamte_Ausgaben
+    st.metric("Ersparnisse/Nicht Ausgegeben", Ersparnisse)
 
-Ersparnisse = Gesamtes_Einkommen - Gesamte_Ausgaben
-df = pd.DataFrame({
-    "Category": ["Projekte", "Verwaltung", "Werbung", "Ersparnisse"],
-    "Amount": [Projekte, Verwaltung, Werbung, Ersparnisse]
-})
-fig = px.pie(df, names='Category', values='Amount', title="Jahres Bilanz Verteilung")
-st.plotly_chart(fig)
+    ## Visualisierung der Ausgaben
+    df = pd.DataFrame({
+        "Category": ["Projekte", "Verwaltung", "Werbung", "Ersparnisse"],
+        "Amount": [Projekte, Verwaltung, Werbung, Ersparnisse]
+    })
+    fig = px.pie(df, names='Category', values='Amount', title="Jahres Bilanz Verteilung")
+    st.plotly_chart(fig)
