7066ff4ff3b0500a907213f98956896a84670550
maikee03 <maike.hermle@web.de>
2025-11-28 13:33:12 +0100

    V11

--- Interactive_Interface.py
+++ Interactive_Interface.py
@@ -5,13 +5,16 @@ import plotly.express as px
 st.set_page_config(layout="wide")
 st.title("Interaktive Finanzübersicht")
 
+COLOR_PROJEKTE ="#0072B2"     # blue
+COLOR_VERWALTUNG = "#D55E00"   # orange
+COLOR_WERBUNG = "#009E73"      # green
 
 col1, col2 = st.columns(2)
 ############ Einnahmen ############
 with col1:
     st.header("Einnahmen")
     ## Interaktive Slider für Einnahmen
-    Gesamtes_Einkommen = st.slider("Gesamte Einnahmen", 0, 7000, 2000)
+    Gesamtes_Einkommen = st.slider("Gesamte Einnahmen", 0, 5000, 2000)
 
     st.metric("Gesamtes Einkommen", Gesamtes_Einkommen)
 
@@ -19,9 +22,9 @@ with col1:
 with col2:
     st.header("Ausgaben")
     ## Interaktive Slider für Ausgaben
-    Projekte = st.slider("Projektausgaben", 0, 2000, 800)
-    Verwaltung = st.slider("Verwaltungsausgaben", 0, 1000, 350)
-    Werbung = st.slider("Werbungsausgaben", 0, 500, 60)
+    Projekte = st.slider(f"🟦 Projekte", 0, 3000, 800)
+    Verwaltung = st.slider(f"🟧 Verwaltung", 0, 1000, 350)
+    Werbung = st.slider(f"🟩 Werbung", 0, 500, 60)
     
     col1, col2 = st.columns(2)
     with col1:
@@ -38,5 +41,5 @@ with col2:
     })
     fig = px.pie(df, names='Category', values='Amount', title="Anteile Ausgaben")
     fig.update_layout(title_font_size=24, title_font_color="black",font=dict(size=18, color="black"))
-    fig.update_traces(marker=dict(colors= ['#636EFA', '#EF553B', '#00CC96'], line=dict(color='#000000', width=2)))
+    fig.update_traces(marker=dict(colors=[COLOR_PROJEKTE, COLOR_VERWALTUNG, COLOR_WERBUNG]))
     st.plotly_chart(fig)
