7bf515b8b5cee7b02ed76492a538a794d97ac76d
maikee03 <maike.hermle@web.de>
2025-11-28 12:37:05 +0100

    V07

--- Interactive_Interface.py
+++ Interactive_Interface.py
@@ -23,15 +23,18 @@ with col2:
     Verwaltung = st.slider("Verwaltungsausgaben", 0, 1000, 350)
     Werbung = st.slider("Werbungsausgaben", 0, 500, 60)
     
+    col1, col2 = st.columns(2)
+    with col1:
     Gesamte_Ausgaben = Projekte + Verwaltung + Werbung
     st.metric("Gesamte Ausgaben", Gesamte_Ausgaben)
+    with col2:
     Ersparnisse = Gesamtes_Einkommen - Gesamte_Ausgaben
     st.metric("Ersparnisse/Nicht Ausgegeben", Ersparnisse)
 
     ## Visualisierung der Ausgaben
     df = pd.DataFrame({
-        "Category": ["Projekte", "Verwaltung", "Werbung", "Ersparnisse"],
-        "Amount": [Projekte, Verwaltung, Werbung, Ersparnisse]
+        "Category": ["Projekte", "Verwaltung", "Werbung"],
+        "Amount": [Projekte, Verwaltung, Werbung]
     })
-    fig = px.pie(df, names='Category', values='Amount', title="Jahres Bilanz Verteilung")
+    fig = px.pie(df, names='Category', values='Amount', title="Anteile Ausgaben")
     st.plotly_chart(fig)
