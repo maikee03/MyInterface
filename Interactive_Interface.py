8e09a7577813c031014a1ab4f833146e79b43e11
maikee03 <maike.hermle@web.de>
2025-11-28 12:11:24 +0100

    V03

--- Interactive_Interface.py
+++ Interactive_Interface.py
@@ -2,20 +2,30 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-income = st.slider("Monthly Income", 0, 7000, 2000) #changed 5000 to 6000
-rent = st.slider("Projekte", 0, 2000, 800)
-groceries = st.slider("Verwaltung", 0, 1000, 350)
-transport = st.slider("Werbung", 0, 500, 60)
+################ SLIDER INPUT ################
 
-expenses = rent + groceries + transport
-savings = income - expenses
+#Einkommen
+Gesamtes_Einkommen = st.slider("Gesamtes Einkommen", 0, 7000, 2000)
 
-st.metric("Ausgaben", expenses)
-st.metric("Ersparnisse", savings)
+#Ausgaben
+Projekte = st.slider("Projektausgaben", 0, 2000, 800)
+Verwaltung = st.slider("Verwaltungsausgaben", 0, 1000, 350)
+Werbung = st.slider("Werbungsausgaben", 0, 500, 60)
+Gesamte_Ausgaben = Projekte + Verwaltung + Werbung
+
+################# Calculations #################
+
+#Bilanz
+Ersparnisse = Gesamtes_Einkommen - Gesamte_Ausgaben
+
+################# Output #################
+st.header("Jahres Bilanz")
+st.metric("Gesamte Ausgaben", Gesamte_Ausgaben) #st.metric gives as a nice formatted output of Label and Value
+#st.metric("Ersparnisse", Ersparnisse)
 
 df = pd.DataFrame({
     "Category": ["Projekte", "Verwaltung", "Werbung", "Ersparnisse"],
-    "Amount": [rent, groceries, transport, savings]
+    "Amount": [Projekte, Verwaltung, Werbung, Ersparnisse]
 })
-fig = px.pie(df, names='Category', values='Amount', title="Monthly Budget")
+fig = px.pie(df, names='Category', values='Amount', title="Jahres Bilanz Verteilung")
 st.plotly_chart(fig)
