d3bc3da58f9753ca4f2491a60124b638c881a91c
maikee03 <maike.hermle@web.de>
2025-11-28 12:19:08 +0100

    V05

--- Interactive_Interface.py
+++ Interactive_Interface.py
@@ -5,27 +5,33 @@ import plotly.express as px
 ################ SLIDER INPUT ################
 
 #Einkommen
-Gesamtes_Einkommen = st.slider("Gesamtes Einkommen", 0, 7000, 2000)
+
 
 #Ausgaben
-Projekte = st.slider("Projektausgaben", 0, 2000, 800)
-Verwaltung = st.slider("Verwaltungsausgaben", 0, 1000, 350)
-Werbung = st.slider("Werbungsausgaben", 0, 500, 60)
-Gesamte_Ausgaben = Projekte + Verwaltung + Werbung
+
 
 ################# Calculations #################
 
 #Bilanz
-Ersparnisse = Gesamtes_Einkommen - Gesamte_Ausgaben
+
 
 ################# Output #################
 st.header("Jahres Bilanz")
 col1, col2 = st.columns(2)
 with col1:
+    Gesamtes_Einkommen = st.slider("Gesamtes Einkommen", 0, 7000, 2000)
+    
     st.metric("Gesamtes Einkommen", Gesamtes_Einkommen)
-with col2:
-    st.metric("Gesamte Ausgaben", Gesamte_Ausgaben)
 
+with col2:
+    Projekte = st.slider("Projektausgaben", 0, 2000, 800)
+    Verwaltung = st.slider("Verwaltungsausgaben", 0, 1000, 350)
+    Werbung = st.slider("Werbungsausgaben", 0, 500, 60)
+    
+    Gesamte_Ausgaben = Projekte + Verwaltung + Werbung
+    st.metric("Gesamte Ausgaben", Gesamte_Ausgaben)
+    
+Ersparnisse = Gesamtes_Einkommen - Gesamte_Ausgaben
 df = pd.DataFrame({
     "Category": ["Projekte", "Verwaltung", "Werbung", "Ersparnisse"],
     "Amount": [Projekte, Verwaltung, Werbung, Ersparnisse]
